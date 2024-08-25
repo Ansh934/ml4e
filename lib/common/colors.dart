028ec7c5dafa8ea2142832d76931598c64fa22b9
Ansh Soni <soni.ansh.king09@gmail.com>
2024-08-25 17:46:36 +0530

    com

--- lib/common/colors.dart
+++ lib/common/colors.dart
@@ -6,4 +6,5 @@ class Colours {
   static const Color primary = Color(0xFF1F2937);
   static const Color background = Color.fromRGBO(3, 7, 18, 1);
   static const Color border = Color.fromRGBO(31, 41, 55, 1);
+  static const Color grid = Color.fromRGBO(21, 25, 35, 1);
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:grid_background/grid_background.dart';
 import 'package:ml4e/common/colors.dart';
 
 class HomePage extends StatelessWidget {
@@ -38,66 +39,79 @@ class HomePage extends StatelessWidget {
             ),
           ),
         ],
-        bottom: const PreferredSize(
-          preferredSize: Size.fromHeight(1),
-          child: Divider(color: Colours.border),
-        ),
+        // bottom: const PreferredSize(
+        //   preferredSize: Size.fromHeight(1),
+        //   child: Divider(color: Colours.border),
+        // ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text(
-                'The road to knowledge starts from here.',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 20),
-              AnimatedTextKit(
-                animatedTexts: [
-                  TyperAnimatedText(
-                    'Learn in-depth Machine Learning with ML4E.',
-                    textAlign: TextAlign.center,
-                    textStyle: const TextStyle(
-                      fontWeight: FontWeight.bold,
+      body: Stack(
+        children: [
+          SizedBox(
+            width: MediaQuery.sizeOf(context).width,
+            height: MediaQuery.sizeOf(context).height,
+            child: const Gridbackground(
+                space: 20,
+                horizontallinewidth: 1.0,
+                verticallinewidth: 1.0,
+                color: Colours.grid),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    'The road to knowledge starts from here.',
+                    style: TextStyle(
                       color: Colors.white,
-                      fontSize: 28,
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 20),
+                  AnimatedTextKit(
+                    animatedTexts: [
+                      TyperAnimatedText(
+                        'Learn in-depth Machine Learning with ML4E.',
+                        textAlign: TextAlign.center,
+                        textStyle: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 28,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  FilledButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      shape: WidgetStatePropertyAll(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      backgroundColor: WidgetStateProperty.all(Colors.white),
+                      padding: WidgetStateProperty.all(
+                        const EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 20,
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      'Learn Now',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
                     ),
                   ),
                 ],
               ),
-              const SizedBox(height: 20),
-              FilledButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  shape: WidgetStatePropertyAll(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  backgroundColor: WidgetStateProperty.all(Colors.white),
-                  padding: WidgetStateProperty.all(
-                    const EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 20,
-                    ),
-                  ),
-                ),
-                child: const Text(
-                  'Learn Now',
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
